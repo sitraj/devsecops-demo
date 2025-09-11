9bd32aaa2a74ecdeb23473969656d0c433169655
Shounak Itraj <shounakitraj@gmail.com>
2025-09-11 23:28:20 +0200

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,23 +4,21 @@ provider "aws" {
 # Get current AWS account info 
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "example" {
-  bucket = "devsecops-demo-bucket-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "devsecops-demo-bucket" {
+  bucket = "devsecops-demo-bucket-${data.aws_caller_identity.current.account_id}" 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "devsecops-demo-security" {
+  bucket = aws_s3_bucket.devsecops-demo-bucket.id
   block_public_acls = true
   block_public_policy = true
   ignore_public_acls = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
 }
